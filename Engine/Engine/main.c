731e467eabb11f58dd1bcdf209acdced9ce5e3f5
Axis Hax <yonathen.habte@gmail.com>
2024-12-11 20:42:25 -0500

    - Update code. Can now create screen.

--- Engine/Engine/main.c
+++ Engine/Engine/main.c
@@ -69,14 +69,76 @@ bool initialize_window(void)
 	return true;
 }
 
+void setup(void)
+{
+
+}
+
+/// <summary>
+/// Process the user's input.
+/// </summary>
+/// <param name="">Void.</param>
+void process_input(void)
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type)
+	{
+		// The event triggered when you click the close button on the window.
+		case SDL_QUIT:
+			// Stop running the application.
+			is_running = false;
+			break;
+		// Triggers on key press.
+		case SDL_KEYDOWN:
+			// If we press the escape key, quit the application.
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+			{
+				is_running = false;
+			}
+			break;
+	}
+}
+
+void update(void)
+{
+
+}
+
+/// <summary>
+/// Render visuals to the screen.
+/// </summary>
+/// <param name=""></param>
+void render(void)
+{
+	// Set the color for drawing to the screen.
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	// Clear the color buffer with the above color.
+	SDL_RenderClear(renderer);
+
+	// Update the screen with the color we chose.
+	SDL_RenderPresent(renderer);
+}
+
+/// <summary>
+/// Entry point of the program.
+/// </summary>
+/// <param name="argc">Number of command-line arguments.</param>
+/// <param name="argv">Command-line arguments.</param>
+/// <returns>0 on successful execution of the program, other values if program execution was not successful.</returns>
 int main(int argc, char* argv[])
 {
 	is_running = initialize_window();
 
+	setup();
+
 	// Render loop. Also called a game loop.
 	while (is_running)
 	{
-
+		process_input();
+		update();
+		render();
 	}
 	
 	return 0;
